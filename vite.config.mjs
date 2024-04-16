4c5a8bfc45264c8118ab0eea820e4058e8f8c299
Vega <obed.vega@teradata.com>
2024-04-16 13:52:07 -0700

    test

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/chatbot-vue-ui/',
   plugins: [
     Vue({
       template: { transformAssetUrls }
